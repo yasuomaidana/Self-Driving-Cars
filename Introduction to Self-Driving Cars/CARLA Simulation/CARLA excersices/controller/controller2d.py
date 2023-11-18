db66101689d86a8b737f311ec6d68fb49cad17b5
yasuomaidana <yasuomaidana@gmail.com>
2023-11-18 19:09:47 +0900

    COUR-1648: Formatting file

--- Introduction to Self-Driving Cars/CARLA Simulation/CARLA excersices/controller/controller2d.py
+++ Introduction to Self-Driving Cars/CARLA Simulation/CARLA excersices/controller/controller2d.py
@@ -7,47 +7,48 @@
 import cutils
 import numpy as np
 
+
 class Controller2D(object):
     def __init__(self, waypoints):
-        self.vars                = cutils.CUtils()
-        self._current_x          = 0
-        self._current_y          = 0
-        self._current_yaw        = 0
-        self._current_speed      = 0
-        self._desired_speed      = 0
-        self._current_frame      = 0
-        self._current_timestamp  = 0
+        self.vars = cutils.CUtils()
+        self._current_x = 0
+        self._current_y = 0
+        self._current_yaw = 0
+        self._current_speed = 0
+        self._desired_speed = 0
+        self._current_frame = 0
+        self._current_timestamp = 0
         self._start_control_loop = False
-        self._set_throttle       = 0
-        self._set_brake          = 0
-        self._set_steer          = 0
-        self._waypoints          = waypoints
-        self._conv_rad_to_steer  = 180.0 / 70.0 / np.pi
-        self._pi                 = np.pi
-        self._2pi                = 2.0 * np.pi
+        self._set_throttle = 0
+        self._set_brake = 0
+        self._set_steer = 0
+        self._waypoints = waypoints
+        self._conv_rad_to_steer = 180.0 / 70.0 / np.pi
+        self._pi = np.pi
+        self._2pi = 2.0 * np.pi
 
     def update_values(self, x, y, yaw, speed, timestamp, frame):
-        self._current_x         = x
-        self._current_y         = y
-        self._current_yaw       = yaw
-        self._current_speed     = speed
+        self._current_x = x
+        self._current_y = y
+        self._current_yaw = yaw
+        self._current_speed = speed
         self._current_timestamp = timestamp
-        self._current_frame     = frame
+        self._current_frame = frame
         if self._current_frame:
             self._start_control_loop = True
 
     def update_desired_speed(self):
-        min_idx       = 0
-        min_dist      = float("inf")
+        min_idx = 0
+        min_dist = float("inf")
         desired_speed = 0
         for i in range(len(self._waypoints)):
             dist = np.linalg.norm(np.array([
-                    self._waypoints[i][0] - self._current_x,
-                    self._waypoints[i][1] - self._current_y]))
+                self._waypoints[i][0] - self._current_x,
+                self._waypoints[i][1] - self._current_y]))
             if dist < min_dist:
                 min_dist = dist
                 min_idx = i
-        if min_idx < len(self._waypoints)-1:
+        if min_idx < len(self._waypoints) - 1:
             desired_speed = self._waypoints[min_idx][2]
         else:
             desired_speed = self._waypoints[-1][2]
@@ -61,7 +62,7 @@ class Controller2D(object):
 
     def set_throttle(self, input_throttle):
         # Clamp the throttle command to valid bounds
-        throttle           = np.fmax(np.fmin(input_throttle, 1.0), 0.0)
+        throttle = np.fmax(np.fmin(input_throttle, 1.0), 0.0)
         self._set_throttle = throttle
 
     def set_steer(self, input_steer_in_rad):
@@ -69,29 +70,29 @@ class Controller2D(object):
         input_steer = self._conv_rad_to_steer * input_steer_in_rad
 
         # Clamp the steering command to valid bounds
-        steer           = np.fmax(np.fmin(input_steer, 1.0), -1.0)
+        steer = np.fmax(np.fmin(input_steer, 1.0), -1.0)
         self._set_steer = steer
 
     def set_brake(self, input_brake):
         # Clamp the steering command to valid bounds
-        brake           = np.fmax(np.fmin(input_brake, 1.0), 0.0)
+        brake = np.fmax(np.fmin(input_brake, 1.0), 0.0)
         self._set_brake = brake
 
     def update_controls(self):
         ######################################################
         # RETRIEVE SIMULATOR FEEDBACK
         ######################################################
-        x               = self._current_x
-        y               = self._current_y
-        yaw             = self._current_yaw
-        v               = self._current_speed
+        x = self._current_x
+        y = self._current_y
+        yaw = self._current_yaw
+        v = self._current_speed
         self.update_desired_speed()
-        v_desired       = self._desired_speed
-        t               = self._current_timestamp
-        waypoints       = self._waypoints
+        v_desired = self._desired_speed
+        t = self._current_timestamp
+        waypoints = self._waypoints
         throttle_output = 0
-        steer_output    = 0
-        brake_output    = 0
+        steer_output = 0
+        brake_output = 0
 
         ######################################################
         ######################################################
@@ -159,12 +160,12 @@ class Controller2D(object):
                 access the persistent variables declared above here. For
                 example, can treat self.vars.v_previous like a "global variable".
             """
-            
+
             # Change these outputs with the longitudinal controller. Note that
             # brake_output is optional and is not required to pass the
             # assignment, as the car will naturally slow down over time.
             throttle_output = 0
-            brake_output    = 0
+            brake_output = 0
 
             ######################################################
             ######################################################
@@ -176,16 +177,16 @@ class Controller2D(object):
                 access the persistent variables declared above here. For
                 example, can treat self.vars.v_previous like a "global variable".
             """
-            
+
             # Change the steer output with the lateral controller. 
-            steer_output    = 0
+            steer_output = 0
 
             ######################################################
             # SET CONTROLS OUTPUT
             ######################################################
             self.set_throttle(throttle_output)  # in percent (0 to 1)
-            self.set_steer(steer_output)        # in rad (-1.22 to 1.22)
-            self.set_brake(brake_output)        # in percent (0 to 1)
+            self.set_steer(steer_output)  # in rad (-1.22 to 1.22)
+            self.set_brake(brake_output)  # in percent (0 to 1)
 
         ######################################################
         ######################################################
